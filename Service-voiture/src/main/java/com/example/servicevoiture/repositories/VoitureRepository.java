2dcbeca65f46190ce40cc5f3f7c7c12f9851f685
SAbde03 <salhiabde03@gmail.com>
2025-11-30 15:17:53 +0100

    Orcherstration ...

---
+++ Service-voiture/src/main/java/com/example/servicevoiture/repositories/VoitureRepository.java
@@ -0,0 +1,10 @@
+package com.example.servicevoiture.repositories;
+
+import com.example.servicevoiture.entities.Voiture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VoitureRepository extends JpaRepository<Voiture, Long> {
+    List<Voiture> findByClientId(Long clientId);
+}
---
+++ Service-voiture/src/main/java/com/example/servicevoiture/services/VoitureService.java
@@ -0,0 +1,18 @@
+package com.example.servicevoiture.services;
+
+import com.example.servicevoiture.entities.Voiture;
+import com.example.servicevoiture.repositories.VoitureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VoitureService {
+    @Autowired
+    private VoitureRepository voitureRepository;
+    public Voiture enregistrerVoiture(Voiture voiture) {
+        return voitureRepository.save(voiture);
+    }
+    public Voiture modifierVoiture(Voiture voiture) {
+        return voitureRepository.save(voiture);
+    }
+}
